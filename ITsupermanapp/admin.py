011e9f2653c3383d8b9fd988f6ba4d7fe25e0506
shogo6768 <shogo6768@gmail.com>
2020-12-24 07:10:43 +0900

    コンフリクト解消

--- ITsupermanapp/admin.py
+++ ITsupermanapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db import models
 from django.contrib.auth.admin import UserAdmin
 from .models import Category, Tag, PostModel, CustomUser
 
@@ -6,8 +7,9 @@ from .models import Category, Tag, PostModel, CustomUser
 class CustomUserAdmin(admin.ModelAdmin):
     filter_horizontal = ('like_post',)
 
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(PostModel)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
